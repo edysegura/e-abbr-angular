91d44795c76a010bf658ce58c28c2921ad60139e
Edy Segura <ednardo@inatel.br>
2018-01-08 23:48:01 -0200

    Using angular event

--- src/app/abbr/abbr.component.ts
+++ src/app/abbr/abbr.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-abbr',
   template: `
-    <input type="text" [value]="abbr" />
+    <input type="text" (input)="onUserInput($event)" [value]="abbr" />
   `
 })
 export class AbbrComponent {
   abbr = 'HTML';
+
+  onUserInput(event) {
+    this.abbr = event.target.value;
+  }
 }
