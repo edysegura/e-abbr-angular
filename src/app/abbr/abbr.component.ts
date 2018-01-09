d521ed07d5ff03003a528d5c80f9c5d8bb0c4700
Edy Segura <ednardo@inatel.br>
2018-01-09 00:10:32 -0200

    Added two way data bind

--- src/app/abbr/abbr.component.ts
+++ src/app/abbr/abbr.component.ts
@@ -3,14 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-abbr',
   template: `
-    <input type="text" (input)="onUserInput($event)" [value]="abbr" />
+    <input type="text" [(ngModel)]="abbr" />
     <p>Input: {{abbr}}</p>
   `
 })
 export class AbbrComponent {
   abbr = 'HTML';
-
-  onUserInput(event) {
-    this.abbr = event.target.value;
-  }
 }
